52c1c6c78012ae1f4a4e6aa663da54b042bad276
Kartikeyagupta5 <kartikeyagupta998@gmail.com>
2024-12-04 06:05:22 +0530

    Fibonaaci

--- Lecture 14 (Recursion)/Fibonacci.java
+++ Lecture 14 (Recursion)/Fibonacci.java
@@ -1,8 +1,11 @@
 public class Fibonacci{
-    public static void printFibo(int a, int b, int n){
-        
+    public static int fibonaaci(int n){  // yeah function usi term ki value dega na ki puri series . for example term 8 = 21 se in function working.
+        if (n == 0 || n == 1)
+        return n;
+
+        return fibonaaci(n-1) + fibonaaci(n-2);  // time compexity = O(2^n) and space complexity = O(n).
     }
     public static void main(String[] args) {
-        printFibo(0, 1, 5);
+        System.out.println(fibonaaci(8));
     }
 }
